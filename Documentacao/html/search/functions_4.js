ee8dbcff3b2486b65e55832e73a74223931fb58e
alaxgabriel <alax_gabriel71@live.com>
2017-11-12 13:10:01 -0200

    HTML copia

---
+++ Documentacao/html/search/functions_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['starting',['starting',['../class_main_window.html#a756fd5832a55713567189c43d8aa27ca',1,'MainWindow']]],
+  ['stopping',['stopping',['../class_main_window.html#a6ba0902dc1552226ae4a7866a42efee0',1,'MainWindow']]]
+];
